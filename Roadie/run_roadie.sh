5315d5379ec43f0ddc7ca5eccfbca49d16627e31
Antonio <cezaravadanei@gmail.com>
2025-10-06 18:24:13 +0200

    running

--- Roadie/run_roadie.sh
+++ Roadie/run_roadie.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
-pkill -f main.py 2>/dev/null
+
+# Kill any running instance of the app
+pkill -f main.py 2>/dev/null || true
+
+# Go to script directory
 cd "$(dirname "$0")"
+
+# Run the app
 python3 main.py
